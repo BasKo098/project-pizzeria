8e009b179b562abf72bce24d5be69c1830eb4de2
Barbara Kosińska <basia7kosinska@gmail.com>
2023-11-13 11:13:34 +0100

    add counting price

--- project-pizzeria/dist/js/script.js
+++ project-pizzeria/dist/js/script.js
@@ -62,21 +62,13 @@ const select = {
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
       thisProduct.processOrder();
-      console.log('new Product:', thisProduct);
     } 
 
     renderInMenu(){
         const thisProduct = this;
-        /*generate HTML based on template*/
         const generatedHTML = templates.menuProduct(thisProduct.data);
-
-        /*create element using utils.createElementFromHTML*/
         thisProduct.element = utils.createDOMFromHTML(generatedHTML);
-
-        /*find menu container*/
         const menuContainer = document.querySelector(select.containerOf.menu);
-
-        /*add element to menu*/
         menuContainer.appendChild(thisProduct.element);
     }
 
@@ -92,31 +84,20 @@ const select = {
 
     initAccordion() {
       const thisProduct = this;
-    
-      /* [DONE] find the clickable trigger (the element that should react to clicking) */
-      //const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-    
-      /* [DONE] START: add event listener to clickable trigger on event click */
       thisProduct.accordionTrigger.addEventListener('click', function(event) {
-        /* [DONE] prevent default action for event */
-        event.preventDefault();
+      event.preventDefault();
+      const activeProduct = document.querySelector(select.all.menuProductsActive);
     
-        /* find active product (product that has active class) */
-        const activeProduct = document.querySelector(select.all.menuProductsActive);
-    
-        /* [DONE] if there is active product and it's not thisProduct.element, remove class active from it */
         if (activeProduct && activeProduct !== thisProduct.element) {
           activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
         }
-        /* [DONE] toggle active class on thisProduct.element */
+        
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
     }
 
     initOrderForm() {
       const thisProduct = this;
-      console.log(thisProduct.initOrderForm);
-      
       thisProduct.form.addEventListener('submit', function(event){
         event.preventDefault();
         thisProduct.processOrder();
@@ -134,24 +115,53 @@ const select = {
       });
     }
 
-    processOrder(){
-      const thisProduct = this;;
-      console.log(thisProduct.processOrder);
+    processOrder() {
+      const thisProduct = this;
+    
+      // [DONE] covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+    
+      // [DONE] set price to default price
+      let price = thisProduct.data.price;
+    
+      // [DONE] for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+        // [DONE] determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+    
+        // [DONE] for every option in this category
+        for(let optionId in param.options) {
+          // [DONE] determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
 
-    }
+          // [DONE] check if there is param with a name of paramId in formData and if it includes optionId
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+
+            // [DONE] check if the option is not default
+            if(!option.default) {
+              price += parseFloat(option.price); 
+              // [DONE] add option price to price variable
+            }
+            } else {
+            // [DONE] check if the option is default
+            if(option.default) {
+              // [DONE] reduce price variable
+              price -= parseFloat(option.price);
+            }
+            } 
+        }
+        // [DONE] update calculated price in the HTML
+        thisProduct.priceElem.innerHTML = price;
+      }
+    }  
   }
   
   const app = {
     initMenu: function(){
       const thisApp = this;
-      console.log('thisApp.data:', thisApp.data);
       for(let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
-      //const thisApp = this;
-      //console.log('thisApp.data:', thisApp.data);
-      //const testProduct = new Product();
-      //console.log('testProduct:', testProduct);
     },  
     
     initData: function(){
