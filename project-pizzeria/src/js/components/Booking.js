75ef3f6cc650f74e76cbbaf06ec32ca782dad2ac
Barbara Kosińska <basia7kosinska@gmail.com>
2023-12-09 17:44:19 +0100

    add getData method in Booking.js class

--- project-pizzeria/src/js/components/Booking.js
+++ project-pizzeria/src/js/components/Booking.js
@@ -1,4 +1,5 @@
-import { select, templates } from '../settings.js';
+import { select, settings, templates } from '../settings.js';
+import {utils} from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
@@ -8,6 +9,64 @@ class Booking {
         const thisBooking = this;
         thisBooking.render(element);
         thisBooking.initWidgets();
+        thisBooking.getData();
+    }
+
+    getData() {
+        const thisBooking = this;
+        const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+        const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
+        const params = {
+            booking: [
+                startDateParam,
+                endDateParam,
+            ],
+            eventCurrent: [
+                settings.db.notRepeatParam,
+                startDateParam,
+                endDateParam,
+            ],
+            eventRepeat: [
+                settings.db.repeatParam,
+                endDateParam,            
+            ],
+        };
+
+        //console.log('getData params', params);
+
+        const urls = {
+            booking:      settings.db.url + '/' + settings.db.bookings 
+                                          + '?' + params.booking.join('&'),
+            eventCurrent: settings.db.url + '/' + settings.db.events 
+                                          + '?' + params.eventCurrent.join('&'),
+            eventRepeat:  settings.db.url + '/' + settings.db.events 
+                                          + '?' + params.eventRepeat.join('&'),
+        };
+
+       //console.log('getData url', urls);
+
+        Promise.all([
+            fetch(urls.booking),
+            fetch(urls.eventCurrent),
+            fetch(urls.eventRepeat),
+        ])
+
+        .then(function(allResponses){
+            const bookingsResponse = allResponses[0];
+            const eventCurrentResponse = allResponses[1];
+            const eventRepeatResponse = allResponses[2];
+            return Promise.all([
+            bookingsResponse.json(),
+            eventCurrentResponse.json(),
+            eventRepeatResponse.json(),
+            ]);
+        })
+        .then(function([bookings, eventCurrent, eventRepeat]){
+           console.log('bookings', bookings);
+           console.log('eventsCurrent', eventCurrent);
+           console.log('eventsRepeat', eventRepeat);
+        });
     }
 
     render(element) {
@@ -20,8 +79,7 @@ class Booking {
         thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.widgets.booking.peopleAmount);
         thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.widgets.booking.hoursAmount);
         thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.amount.datePicker.wrapper);
-        thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.amount.hourPicker.wrapper);
-        
+        thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.amount.hourPicker.wrapper);    
     }   
 
     initWidgets() {
@@ -32,10 +90,10 @@ class Booking {
         thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
         thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
         
-        thisBooking.dom.peopleAmount.addEventListener('click', function(event) {
+        thisBooking.dom.peopleAmount.addEventListener('updated', function(event) {
             event.preventDefault();
         })
-        thisBooking.dom.hoursAmount.addEventListener('click', function(event) {
+        thisBooking.dom.hoursAmount.addEventListener('updated', function(event) {
             event.preventDefault(); 
         });
     }
